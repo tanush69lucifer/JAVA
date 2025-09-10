f09878045edc864c6d098c63d4e3223348bfadf2
tanush69lucifer <tanushbharadwaj2514@gmail.com>
2025-09-10 11:30:49 +0530

    Update unique_occurances.java

--- Java_Tutorials/GFG_practice/unique_occurances.java
+++ Java_Tutorials/GFG_practice/unique_occurances.java
@@ -3,16 +3,13 @@ import java.util.*;
 class Solution {
     public static boolean isFrequencyUnique(int n, int[] arr) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        for (int s : arr) {
-            map.put(s, map.getOrDefault(s, 0) + 1);
+        HashSet<Integer> set = new HashSet<>();
+        for (int i : arr) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
         }
-        HashSet<Integer> freqSet = new HashSet<>();
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            int freq = entry.getValue();
-            if (!freqSet.add(freq)) {
-                return false;
-            }
+        for (int i:map.values()){
+            set.add(i);
         }
-        return true;
+        return (map.size()==set.size());
     }
 }
