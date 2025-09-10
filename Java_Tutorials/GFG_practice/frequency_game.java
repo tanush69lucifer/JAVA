2bfb51e93e8cc46e9fdc185d9e87a45e7f498edb
tanush69lucifer <tanushbharadwaj2514@gmail.com>
2025-09-10 11:18:12 +0530

    Update frequency_game.java

--- Java_Tutorials/GFG_practice/frequency_game.java
+++ Java_Tutorials/GFG_practice/frequency_game.java
@@ -1,11 +1,18 @@
 import java.util.*;
-public class FrequencyGame {
-    public static void main(String[] args) {
-        int[] arr = {1,2,2,3,3,3};
-        HashMap<Integer,Integer> freq = new HashMap<>();
-        for(int num : arr) freq.put(num, freq.getOrDefault(num,0)+1);
-        int max = Collections.max(freq.values());
-        System.out.println("Max frequency elements: ");
-        for(int k : freq.keySet()) if(freq.get(k)==max) System.out.println(k);
+
+class Solution {
+    public static int LargButMinFreq(int arr[], int n) {
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        for (int num : arr) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+        int minFreq = Collections.min(freq.values());
+        int ans = Integer.MIN_VALUE;
+        for (int key : freq.keySet()) {
+            if (freq.get(key) == minFreq) {
+                ans = Math.max(ans, key);
+            }
+        }
+        return ans;
     }
 }
