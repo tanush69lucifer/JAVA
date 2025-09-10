af18f139db1a64e3a56702a49978dadc0c40c97d
tanush69lucifer <tanushbharadwaj2514@gmail.com>
2025-09-10 11:22:50 +0530

    Update Distinct_substrings.java

--- Java_Tutorials/GFG_practice/Distinct_substrings.java
+++ Java_Tutorials/GFG_practice/Distinct_substrings.java
@@ -1,13 +1,10 @@
-import java.util.*;
-public class DistinctSubstrings {
-    public static void main(String[] args) {
-        String s = "abc";
-        HashSet<String> set = new HashSet<>();
-        for(int i=0;i<s.length();i++){
-            for(int j=i+1;j<=s.length();j++){
-                set.add(s.substring(i,j));
-            }
+class Solution {
+    int fun(String s) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length() - 1; i++) {
+            String word = s.substring(i, i + 2); 
+            map.put(word, map.getOrDefault(word, 0) + 1);
         }
-        System.out.println("Distinct substrings: " + set.size());
+        return map.size(); 
     }
 }
